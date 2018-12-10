4a2e52c89bd525915e6e40b0ec5544e673be23ae
CCastro <chris502castro@gmail.com>
2018-12-10 16:54:14 -0700

    Added descriptions/comments, fixed the console.log's for the spotify(music) function.

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+// Node Config
 require("dotenv").config();
 var axios = require("axios");
 var moment = require("moment");
@@ -9,8 +10,11 @@ var spotify = new Spotify({
     secret: secretKey.spotify.secret
 });
 
+// Global Variables set from command line arguments
 var [a, b, ...c] = process.argv;
 var [d, ...e] = c;
+
+// Conditional to direct traffic depending on what global variables were set to
 if (d === "concert-this" || d === "spotify-this-song" || d === "movie-this" || d === "do-what-it-says") {
     if (d === "concert-this") {
         bandsInTown();
@@ -25,37 +29,36 @@ if (d === "concert-this" || d === "spotify-this-song" || d === "movie-this" || d
     console.log(`Error: \n"${d}" is not an option`)
 }
 
+// If the search term is greater than or equal to 1 word in length, search based on the user command; else, if the search term is not defined, automatically pull up Ariana Grande concert.
 function bandsInTown() {
     if (e.length >= 1) {
         var artist = e.join("+");
         axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function (response) {
             var event = response.data[0]
             if (event) {
-                console.log(`\nSoonest available playing: \n\nVenue Name: ${event.venue.name}\nLocation: ${event.venue.city}, ${event.venue.region}\n`);
                 if (moment(event.datetime).isValid()) {
                     var time1 = moment(event.datetime).format("Do MMMM YYYY");
                     var time2 = moment(event.datetime).format("hh:mm a");
-                    console.log(`Date: ${time1} at ${time2}`);
+                    console.log(`\nSoonest available playing for ${e.join(" ")}: \n\nVenue Name: ${event.venue.name}\nLocation: ${event.venue.city}, ${event.venue.region}\nLineup: ${event.lineup.join(", ")}\nDate: ${time1} at ${time2}`);
                 } else {
                     console.log(`Error processing time: direct from object ${event.datetime}`);
                 }
             } else {
                 console.log(`No found tour dates for ${e.join(" ")}`);
             }
-        }
-        );
+        });
     } else {
         axios.get("https://rest.bandsintown.com/artists/ariana+grande/events?app_id=codingbootcamp").then(function (response) {
             var event = response.data[0]
-            console.log(`\nSoonest available playing: \n\nVenue Name: ${event.venue.name}\nLocation: ${event.venue.city}, ${event.venue.region}\n`);
             var time1 = moment(event.datetime).format("Do MMMM YYYY");
             var time2 = moment(event.datetime).format("hh:mm a");
-            console.log(`Date: ${time1} at ${time2}`);
+            console.log(`\nSoonest available playing for Ariana Grande: \n\nVenue Name: ${event.venue.name}\nLocation: ${event.venue.city}, ${event.venue.region}\nLineup: ${event.lineup.join(", ")}\nDate: ${time1} at ${time2}`);
         })
     }
 
 }
 
+// If the search term isn't defined by the user, display to the user the information for The Sign, by Ace of Base; else, if it is defined, display the first spotify attempt to grab the user-defined song.
 function music() {
     if (e.length === 0) {
         spotify.search({ type: 'track', query: 'The Sign' }).then(function (response) {
@@ -92,6 +95,7 @@ function music() {
 
 }
 
+// If the user defines the search term, display the OMDB information on it; else, if the user does not define a search term, search for Riddick
 function oMDB() {
     if (e.length >= 1) {
         var movie = e.join("+")
@@ -102,7 +106,7 @@ function oMDB() {
         }
         );
     } else {
-        axios.get("http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=trilogy").then(function (response) {
+        axios.get("http://www.omdbapi.com/?t=riddick&y=&plot=short&apikey=trilogy").then(function (response) {
             var movieInfo = response.data;
             var release = moment(movieInfo.Released, "DD MMMM YYYY").format("YYYY");
             console.log(`\n\nMovie Title: ${movieInfo.Title}\nYear of Release: ${release}\nIMDB Rating: ${movieInfo.Ratings[0].Value}\nRotten Tomatoes Rating: ${movieInfo.Ratings[1].Value}\nOrigin of Production: ${movieInfo.Country}\nLanguage: ${movieInfo.Language}\nPlot: ${movieInfo.Plot}\nActors: ${movieInfo.Actors}\n\n`);
@@ -111,6 +115,7 @@ function oMDB() {
 
 }
 
+// Search based on what's stored first in the random.txt file
 function standard() {
     fs.readFile("random.txt", "utf8", function(error, data) {
         if (error) {
